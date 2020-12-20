0560cf80eeae63a122e8590d269c8b5460909920
caffix <caffix@github.com>
2020-12-19 22:30:02 -0500

    Added the SinkFunc

--- sink.go
+++ sink.go
@@ -8,3 +8,11 @@ type OutputSink interface {
 	// a Pipeline instance.
 	Consume(context.Context, Data) error
 }
+
+// SinkFunc is an adapter to allow the use of plain functions as OutputSink instances.
+type SinkFunc func(context.Context, Data) error
+
+// Consume calls f(ctx, data)
+func (f SinkFunc) Consume(ctx context.Context, data Data) error {
+	return f(ctx, data)
+}
